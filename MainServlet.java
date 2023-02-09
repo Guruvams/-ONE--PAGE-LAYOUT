7263fe2ec8ef41c61bf43c128d023796e5d4dcfc
Guruvamsi <110326186+Guruvams@users.noreply.github.com>
2023-02-09 23:34:11 +0530

    Create MainServlet.java

---
+++ MainServlet.java
@@ -0,0 +1,20 @@
+package com.QSP.Login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet
+{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		resp.sendRedirect("http://localhost:8080/Mock-App/select.html");
+	}
+}
